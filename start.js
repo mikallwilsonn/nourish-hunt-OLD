f31594b0a0d1fcd8c4e467a11827ff394ec21b14
Michael Wilson <info@michaelwilsion.io>
2018-06-21 21:12:27 -0400

    Integrated Forest Admin

--- start.js
+++ start.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose' );
 
 // import environmental variables from our variables.env file
-require('dotenv').config({ path: 'variables.env' });
+require( 'dotenv' ).config({ path: 'variables.env' });
 
 // Connect to our Database and handle any bad connections
-mongoose.connect(process.env.DATABASE);
+mongoose.connect( process.env.DATABASE );
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connection.on('error', (err) => {
-  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+mongoose.connection.on( 'error', ( err ) => {
+  console.error( `🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
 // READY?! Let's go!
@@ -19,8 +19,8 @@ require( './models/Review' );
 
 
 // Start our app!
-const app = require('./app');
-app.set('port', process.env.PORT || 7777);
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
+const app = require( './app' );
+app.set( 'port', process.env.PORT || 7777 );
+const server = app.listen( app.get( 'port' ), () => {
+  console.log( `Express running → PORT ${server.address().port}` );
 });
