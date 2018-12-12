5a677a5a8022cb7bba788ef698527433317d18b6
Michael Wilson <info@michaelwilsion.io>
2018-12-12 02:37:37 -0500

    Fixed routing issue that caused a 404 when redirecting after saving updated store info

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -103,7 +103,7 @@ exports.updateStore = async ( req, res ) => {
         runValidators: true, 
     }).exec();
     req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store -></a>`);
-    res.redirect( `/stores/${store._id}` );
+    res.redirect( `/stores/${store.slug}` );
     // redirect to store and tell them it worked
 }
 
